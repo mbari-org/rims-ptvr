4bdf26e1e5f92c439b7c6f9440f7d97bb76620d0
rimsadmin <rimsadmin@deeprip>
2023-03-21 16:57:48 -0700

    revise test folders

--- rois/tests.py
+++ rois/tests.py
@@ -1,9 +1,12 @@
+import glob
 from django.test import TransactionTestCase
 from scripts.import_images_from_dir import do_import, run
 
 # Create your tests here.
 class ImportTestCase(TransactionTestCase):
-    run('/nvme-pool/rims/image_import/test_ayeris_to_rims', 'rois/default_proc_settings.json')
+    dl = glob.glob('/nvme-pool/rims/image_import/dec-2022-c1-m2/extracted_rois/*')
+    for d in dl:
+        run(d, 'rois/default_proc_settings.json')
 
 #class LargeImportTestCase(TestCase):
 #    run('/home/rimsadmin/image_import', 'rois/default_proc_settings.json')
